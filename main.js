cd1967d354a4a30e53d1f5e4ec12923bd1717629
Ramjan Ali <ramjancse@gmail.com>
2021-02-04 16:17:23 +0600

    Write a function to get sum of two number

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ $(document).ready(function() {
 $("#button1").click(function() {
     $('#card1').slideToggle("slow");
 });
+
+
+$(document).ready(function() {
+    $('#card2').hide();
+});
+$("#button2").click(function() {
+    $('#card2').slideToggle("slow");
+});
--- script.js
+++ script.js
@@ -2,31 +2,44 @@
 
 var date =  new Date();
 var today = date.getDay();
-var currentTime = date.toLocaleTimeString();
 var currentDate = date.toLocaleDateString();
+var currentTime = date.toLocaleTimeString();
+var currentDtaeAndTime = date.toLocaleString('en-us', { day: 'numeric', year: 'numeric', month:'long', weekday: 'long',   hour:'numeric', minute:'numeric'})
+var card1view = document.getElementById('answerView1');
+card1view.innerText = "The answer is: " + currentDtaeAndTime;
 
 
 
-if(today === 0){
-    console.log("Today is : Sunday" + "Date: " + currentDate + " Time:" + currentTime);
-}else if(today === 1){
+
+// if(today === 0){
+//     console.log("Today is : Sunday" + "Date: " + currentDate + " Time:" + currentTime);
+// }else if(today === 1){
    
-    console.log("Today is : Monday" + "Date: " + currentDate + " Time:" + currentTime);
-}
-else if(today === 2){
-    console.log("Today is : Tuesday " + "Date: " + currentDate + " Time:" + currentTime);
-}
-else if(today === 3){
-    console.log("Today is : Wednesday " + "Date: " + currentDate + " Time:" + currentTime);
-}
-else if(today === 4){
-    console.log("Today is : Thursday " + "Date: " + currentDate + " Time:" + currentTime);
-}
+//     console.log("Today is : Monday" + "Date: " + currentDate + " Time:" + currentTime);
+// }
+// else if(today === 2){
+//     console.log("Today is : Tuesday " + "Date: " + currentDate + " Time:" + currentTime);
+// }
+// else if(today === 3){
+//     console.log("Today is : Wednesday " + "Date: " + currentDate + " Time:" + currentTime);
+// }
+// else if(today === 4){
+//     console.log("Today is : Thursday " + "Date: " + currentDate + " Time:" + currentTime);
+// }
 
-else if(today === 5){
-    console.log("Today is : Friday " + "Date: " + currentDate + " Time:" + currentTime);
-}
+// else if(today === 5){
+//     console.log("Today is : Friday " + "Date: " + currentDate + " Time:" + currentTime);
+// }
 
-else{
-    console.log("Today is : Friday " + "Date: " + currentDate + " Time:" + currentTime);
+// else{
+//     console.log("Today is : Friday " + "Date: " + currentDate + " Time:" + currentTime);
+// }
+
+
+// 2.  Write a function to get sum of two number;
+
+var addTowNumbers= function(a,b){
+    return a+b;
 }
+const card1view2 = document.getElementById('answerView2');
+card1view2.innerText = "The result is: " + addTowNumbers(10,20);
